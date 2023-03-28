bc42c20eca7890bbd09c2ae415071f307c951144
Ruslan Uteshev <r.s.uteshev@gmail.com>
2023-03-29 01:16:31 +0400

    refaactor: refactor profile component

--- hw_1/src/Components/Profile/Profile.jsx
+++ hw_1/src/Components/Profile/Profile.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import Button from "../Button/Button";
+import { profileFields } from "../../utils/data";
 import './profile.css'
 
 
@@ -8,24 +9,13 @@ export default class Profile extends React.Component {
     <div className="profile">
 
       <h1>{props.data.firstname} {props.data.lastname}</h1>
-
-      <h5 className="profile__subtitle">Дата рождения:</h5>
-      <p className="profile__text">{props.data.birthdate}</p>
-
-      <h5 className="profile__subtitle">Телефон:</h5>
-      <p className="profile__text">{props.data.phone}</p>
-
-      <h5 className="profile__subtitle">Веб-сайт:</h5>
-      <p className="profile__text">{props.data.site}</p>
-
-      <h5 className="profile__subtitle">О себе:</h5>
-      <p className="profile__text">{props.data.about}</p>
-
-      <h5 className="profile__subtitle">Стек технологий:</h5>
-      <p className="profile__text">{props.data.stack}</p>
+      {profileFields.map((item) => (
+        <React.Fragment>
+          <h5 className="profile__subtitle">{item.label}:</h5>
+          <p className="profile__text">{props.data[item.name]}</p>
+        </React.Fragment>
+      ))}
       
-      <h5 className="profile__subtitle">Описание последнего проекта:</h5>
-      <p className="profile__text">{props.data.last_project}</p>
       <Button className="btn-back" text="Назад" type="button" isDisabled={false} key='btn-reset' onClick={props.onReturn} />
     </div>
     )
